d68f0f424d6a47f3cbac171f9b02cc746a60d72c
mariamjaludi <mariam.jaludi@gmail.com>
2019-09-11 16:12:07 -0700

    redirects hooked up

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-//get input in form
-document.querySelector('#my-form').addEventListener('submit', e => {
-  e.preventDefault()
-  //get form values
-  const email = document.querySelector('#b-email').value
-  const bSizeEl = document.getElementById('b-size')
-  const bSize = bSizeEl.options[bSizeEl.selectedIndex].value
-  //radio inputs
+const formEl = document.querySelector("#my-form")
+
+//function to redirect page after form is submitted
+function redirecting(bSize, importanceIndicator){
+  if (bSize == "1-10" || importanceIndicator == "Document Storage" || importanceIndicator == "Full Text Search" || importanceIndicator == "Price"){
+    window.location.href = "/unqualifiedLead.html"
+  } else{
+    window.location.href = "/qualifiedLead.html"
+  }
+}
+
+formEl.addEventListener("submit", e => {
+  e.preventDefault();
+  // get form values
+  const email = document.querySelector("#b-email").value;
+  const bSizeEl = document.getElementById("b-size");
+  const bSize = bSizeEl.options[bSizeEl.selectedIndex].value;
+  // radio inputs
   let rdEls = [
     document.getElementById("rd1"),
     document.getElementById("rd2"),
@@ -15,10 +25,23 @@ document.querySelector('#my-form').addEventListener('submit', e => {
     document.getElementById("rd6"),
     document.getElementById("rd7"),
     document.getElementById("rd8")
-  ]
-  //get checked element
-  let checkedRdEl = rdEls.filter(rdEl => rdEl.checked == true)
-  const checkedRd = checkedRdEl[0].value
+  ];
+  // get checked element
+  let checkedRdEl = rdEls.filter(rdEl => rdEl.checked == true);
+  const importanceIndicator = checkedRdEl[0].value;
 
+  //regex to check if email is valid format
+  let validEmailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+  //regex to check if email is gmail, hotmail, or yahoo
+  let personalEmail = /([a-zA-Z0-9]+)([\.{1}])?([a-zA-Z0-9]+)\@(gmail|hotmail|yahoo|)([\.])com/g.test(email)
+  let qualified = false
+  // if (validEmailFormat && !personalEmail){
+  //
+  //   alert("email is valid")
+  // } else {
+  //   alert("email is not valid")
+  // }
 
-})
+  redirecting(bSize, importanceIndicator);
+
+});
